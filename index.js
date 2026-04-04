4d7cc7a12c6a58b53028983a1b2613f91431b878
ratib54 <ratibsway@gmail.com>
2026-04-04 11:33:28 +0600

    Ready server for deploy

--- index.js
+++ index.js
@@ -98,8 +98,6 @@ async function run() {
    res.send(result);
   })
 
-  await client.db("admin").command({ ping: 1 });
-  console.log("Pinged your deployment. You successfully connected to MongoDB!");
  } finally {
  }
 }
